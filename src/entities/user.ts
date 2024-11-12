454d969d2cf54f41937ae418507ffe7f88f25918
Joshua Tahanlangit <contact@thisjt.me>
2024-11-13 02:57:13 +0800

    refactor: abstract optional to types

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,11 +1,15 @@
 import type { User } from '@prisma/client';
 import type { Prisma } from '@prisma/client';
-type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+import type { Optional } from '$lib/utils/types';
 
 export type UserCreateInput = Optional<Prisma.UserCreateInput, 'salt'>;
 
 export type UserRead = User;
 
+export type UserWithoutPassword = Optional<UserRead, 'password' | 'salt'>;
+
 export type UserUpdateInput = Prisma.UserUpdateInput;
 
 export type UserDeleteWhere = Prisma.UserWhereUniqueInput;
+
+export type UserLoginInput = Pick<UserCreateInput, 'username' | 'password'>;
