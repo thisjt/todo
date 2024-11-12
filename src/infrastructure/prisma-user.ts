539e606ac8b9dbb1c037e1ffc876349c05d5640f
Joshua Tahanlangit <contact@thisjt.me>
2024-11-13 02:17:06 +0800

    refactor: implement eror catch for repo

--- src/infrastructure/prisma-user.ts
+++ src/infrastructure/prisma-user.ts
@@ -1,41 +1,62 @@
 import type { IUserRepository } from '../interface/user-repository';
 import { PrismaClient } from '@prisma/client';
 import type { UserCreateInput, UserDeleteWhere, UserRead, UserUpdateInput } from '../entities/user';
+import { DatabaseError } from '../entities/errors';
 
 export class PrismaUserRepository implements IUserRepository {
 	constructor(private prisma: PrismaClient) {}
 
 	async create(input: UserCreateInput): Promise<UserRead> {
-		return this.prisma.user.create({
-			data: {
-				...input,
-				salt: input.salt ?? ''
-			}
-		});
+		try {
+			return this.prisma.user.create({
+				data: {
+					...input,
+					salt: input.salt ?? ''
+				}
+			});
+		} catch (error) {
+			throw new DatabaseError('Failed to create user', { cause: error });
+		}
 	}
 
 	async findById(id: number): Promise<UserRead | null> {
-		return this.prisma.user.findUnique({
-			where: { id }
-		});
+		try {
+			return this.prisma.user.findUnique({
+				where: { id }
+			});
+		} catch (error) {
+			throw new DatabaseError('Failed to find user by id', { cause: error });
+		}
 	}
 
 	async findByUsername(username: string): Promise<UserRead | null> {
-		return this.prisma.user.findUnique({
-			where: { username }
-		});
+		try {
+			return this.prisma.user.findUnique({
+				where: { username }
+			});
+		} catch (error) {
+			throw new DatabaseError('Failed to find user by username', { cause: error });
+		}
 	}
 
 	async update(id: number, input: UserUpdateInput): Promise<UserRead> {
-		return this.prisma.user.update({
-			where: { id },
-			data: input
-		});
+		try {
+			return this.prisma.user.update({
+				where: { id },
+				data: input
+			});
+		} catch (error) {
+			throw new DatabaseError('Failed to update user', { cause: error });
+		}
 	}
 
 	async delete(input: UserDeleteWhere): Promise<UserRead> {
-		return this.prisma.user.delete({
-			where: input
-		});
+		try {
+			return this.prisma.user.delete({
+				where: input
+			});
+		} catch (error) {
+			throw new DatabaseError('Failed to delete user', { cause: error });
+		}
 	}
 }
