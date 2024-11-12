0575e20e5573b7a489c74b8ceee882a08c120b00
Joshua Tahanlangit <contact@thisjt.me>
2024-11-13 02:46:53 +0800

    refactor: add a findall interface

--- src/interface/user-repository.ts
+++ src/interface/user-repository.ts
@@ -1,14 +1,10 @@
-import type {
-	UserCreateInput,
-	UserDeleteInput,
-	UserEntity,
-	UserUpdateInput
-} from '../entities/user';
+import type { UserCreateInput, UserDeleteWhere, UserRead, UserUpdateInput } from '../entities/user';
 
 export interface IUserRepository {
-	create(input: UserCreateInput): Promise<UserEntity>;
-	findById(id: number): Promise<UserEntity | null>;
-	findByUsername(username: string): Promise<UserEntity | null>;
-	update(id: number, input: UserUpdateInput): Promise<UserEntity>;
-	delete(input: UserDeleteInput): Promise<UserEntity>;
+	create(input: UserCreateInput): Promise<UserRead>;
+	findById(id: number): Promise<UserRead | null>;
+	findByUsername(username: string): Promise<UserRead | null>;
+	findAll(): Promise<UserRead[]>;
+	update(id: number, input: UserUpdateInput): Promise<UserRead>;
+	delete(input: UserDeleteWhere): Promise<UserRead>;
 }
