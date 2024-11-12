6c61b66221ab103d37f4c08a87d677b2ecbfe0e2
Joshua Tahanlangit <contact@thisjt.me>
2024-11-13 02:57:43 +0800

    refactor: add login in controller

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,8 +1,33 @@
-import type { UserCreateInput, UserUpdateInput, UserDeleteWhere } from '../entities/user';
+import type {
+	UserCreateInput,
+	UserUpdateInput,
+	UserDeleteWhere,
+	UserLoginInput,
+	UserWithoutPassword
+} from '../entities/user';
 import { UserApplication } from '../application/user';
+import { NotFoundError, UnauthorizedError } from '../entities/errors';
+import { comparePassword } from '$lib/utils/password';
 
 export class UserController {
 	constructor(private userApplication: UserApplication) {}
+
+	async login(input: UserLoginInput) {
+		const user = await this.userApplication.findByUsername(input.username);
+		if (!user) {
+			throw new NotFoundError('User not found');
+		}
+		const isPasswordValid = comparePassword(input.password, user.salt, user.password);
+		if (!isPasswordValid) {
+			throw new UnauthorizedError('Invalid password');
+		}
+		const userWithoutPassword = user as UserWithoutPassword;
+
+		delete userWithoutPassword.password;
+		delete userWithoutPassword.salt;
+
+		return userWithoutPassword;
+	}
 
 	async create(input: UserCreateInput) {
 		return this.userApplication.create(input);
