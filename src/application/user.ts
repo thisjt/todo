4d010c18d3acaf98c99a1c6095fd9309fc446b32
Joshua Tahanlangit <contact@thisjt.me>
2024-11-13 01:37:50 +0800

    feat: integrate user and hash in user application

---
+++ src/application/user.ts
@@ -0,0 +1,37 @@
+import type {
+	UserCreateInput,
+	UserDeleteInput,
+	UserEntity,
+	UserUpdateInput
+} from '../entities/user';
+import { hashPassword } from '../lib/utils/password';
+import type { IUserRepository } from '../interface/user-repository';
+
+export class UserApplication {
+	constructor(private userRepository: IUserRepository) {}
+
+	async create(input: UserCreateInput): Promise<UserEntity> {
+		const hashedPassword = await hashPassword(input.password);
+		return this.userRepository.create({
+			...input,
+			password: hashedPassword.hashedPassword,
+			salt: hashedPassword.salt
+		});
+	}
+
+	async findById(id: number): Promise<UserEntity | null> {
+		return this.userRepository.findById(id);
+	}
+
+	async findByUsername(username: string): Promise<UserEntity | null> {
+		return this.userRepository.findByUsername(username);
+	}
+
+	async update(id: number, input: UserUpdateInput): Promise<UserEntity> {
+		return this.userRepository.update(id, input);
+	}
+
+	async delete(input: UserDeleteInput): Promise<UserEntity> {
+		return this.userRepository.delete(input);
+	}
+}
