1da9b3ab9dfa63107943dbd9624fc5f23898405a
Joshua Tahanlangit <contact@thisjt.me>
2024-11-13 01:36:07 +0800

    refactor: update user entity, optional salt on create

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,10 +1,11 @@
 import type { User } from '@prisma/client';
 import type { Prisma } from '@prisma/client';
+type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
 
 export type UserEntity = User;
 
-export type UserCreateInput = Prisma.UserCreateInput;
+export type UserCreateInput = Optional<Prisma.UserCreateInput, 'salt'>;
 
 export type UserUpdateInput = Prisma.UserUpdateInput;
 
-export type UserDeleteInput = Partial<UserEntity>;
+export type UserDeleteInput = Prisma.UserWhereUniqueInput;
