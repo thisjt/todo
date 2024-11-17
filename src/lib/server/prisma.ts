0c0f39dc3e6533f0ade96d3e5f662d6b0d73c86e
Joshua Tahanlangit <contact@thisjt.me>
2024-11-17 14:42:29 +0800

    refactor: use d1 database in context

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaD1 } from '@prisma/adapter-d1';
 
-export const prisma = new PrismaClient();
+let d1context: PrismaClient;
+
+export const prismaD1 = (d1: D1Database) => {
+	if (d1context) return d1context;
+
+	const adapter = new PrismaD1(d1);
+
+	d1context = new PrismaClient({ adapter });
+	return d1context;
+};
