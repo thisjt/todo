aedd439be39b1d0686d7c22569fa94dc39e58bb5
Joshua Tahanlangit <contact@thisjt.me>
2024-11-26 17:25:34 +0800

    refactor: have find return null if not found

--- src/application/repositories/Users.ts
+++ src/application/repositories/Users.ts
@@ -2,8 +2,8 @@ import type { Users } from '$src/domain/entities/Users';
 
 export interface IUsersRepository {
 	create(users: Users): Promise<void>;
-	findUsername(username: string): Promise<Users>;
-	findId(id: number): Promise<Users>;
+	findUsername(username: string): Promise<Users | null>;
+	findId(id: number): Promise<Users | null>;
 	update(users: Users): Promise<Users>;
 	delete(id: number): Promise<void>;
 }
