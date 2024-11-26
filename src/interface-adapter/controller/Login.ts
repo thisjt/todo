45451f37dcd5c101d4ed7e719335cb8213627bd2
Joshua Tahanlangit <contact@thisjt.me>
2024-11-26 21:12:17 +0800

    refactor: change path of loginusecase

--- src/interface-adapter/controller/Login.ts
+++ src/interface-adapter/controller/Login.ts
@@ -3,7 +3,7 @@ import { createRoute } from '@hono/zod-openapi';
 import { jsonContentRequired } from 'stoker/openapi/helpers';
 import * as StatusCodes from 'stoker/http-status-codes';
 
-import { LoginUseCase } from '$src/application/use-cases/authentication/Login';
+import { LoginUseCase } from '../../application/use-cases/authentication/Login';
 
 const LoginSchema = z.object({
 	username: z.string().openapi({
