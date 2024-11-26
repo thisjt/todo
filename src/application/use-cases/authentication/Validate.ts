bdae0ee1587b6ac5afe2503169d2c843dcbda527
Joshua Tahanlangit <contact@thisjt.me>
2024-11-26 17:43:25 +0800

    feat: validate use case

---
+++ src/application/use-cases/authentication/Validate.ts
@@ -0,0 +1,25 @@
+import { AuthenticationError } from '$src/domain/entities/errors';
+
+import type { IAuthenticationService } from '$src/application/services/AuthenticationService';
+import type { IUsersRepository } from '$src/application/repositories/Users';
+
+export class ValidateUseCase {
+	constructor(
+		private authenticationService: IAuthenticationService,
+		private usersRepository: IUsersRepository
+	) {}
+
+	async execute(token: string, deepVerify?: boolean) {
+		const session = this.authenticationService.validateSession(token);
+
+		if (!session) throw new AuthenticationError('Invalid Session Token');
+
+		if (!deepVerify) return true;
+
+		const user = await this.usersRepository.findId(session.id);
+
+		if (!user) throw new AuthenticationError('Unknown User');
+
+		return true;
+	}
+}
