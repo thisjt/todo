9597963d199164db30c123a6a05ce27b8f3429f8
Joshua Tahanlangit <contact@thisjt.me>
2024-11-27 01:19:22 +0800

    feat: readmany use case

---
+++ src/application/usecases/todo/ReadFromUser.ts
@@ -0,0 +1,10 @@
+import type { ITodoRepository } from '$src/application/repositories/Todo';
+
+export class ReadManyTodoUseCase {
+	constructor(private todoRepository: ITodoRepository) {}
+
+	async execute(userId: number) {
+		const todo = await this.todoRepository.findFromUsers(userId);
+		return todo;
+	}
+}
