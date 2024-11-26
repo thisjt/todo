4b314958a967416ec03481ef1dbecaa0950d93da
Joshua Tahanlangit <contact@thisjt.me>
2024-11-27 01:25:54 +0800

    feat: delete todo use case

---
+++ src/application/usecases/todo/Delete.ts
@@ -0,0 +1,9 @@
+import type { ITodoRepository } from '$src/application/repositories/Todo';
+
+export class DeleteTodoUseCase {
+	constructor(private todoRepository: ITodoRepository) {}
+
+	async execute(id: number) {
+		await this.todoRepository.delete(id);
+	}
+}
