58b49d7a65197e6122e96ee524854cb83b8da0c8
Joshua Tahanlangit <contact@thisjt.me>
2024-11-26 20:42:34 +0800

    refactor: bind dependencies to di container

--- src/di/modules/authenticationModule.ts
+++ src/di/modules/authenticationModule.ts
@@ -1,6 +1,9 @@
 import type { Container } from '@evyweb/ioctopus';
 
 import { AuthenticationService } from '$src/infrastructure/services/AuthenticationService';
+import { LoginUseCase } from '$src/application/use-cases/authentication/Login';
+import { LoginController } from '$src/interface-adapter/controller/Login';
+import { UsersRepository } from '$src/infrastructure/repositories/Users';
 
 import { DI_SYMBOLS } from '../types';
 
@@ -8,4 +11,14 @@ export function registerAuthenticationModule(container: Container) {
 	container
 		.bind(DI_SYMBOLS.IAuthenticationService)
 		.toClass(AuthenticationService, [DI_SYMBOLS.Context]);
+
+	container
+		.bind(DI_SYMBOLS.IUsersRepository)
+		.toClass(UsersRepository, [DI_SYMBOLS.Context, DI_SYMBOLS.IAuthenticationService]);
+
+	container
+		.bind(DI_SYMBOLS.ILoginUseCase)
+		.toClass(LoginUseCase, [DI_SYMBOLS.IAuthenticationService, DI_SYMBOLS.IUsersRepository]);
+
+	container.bind(DI_SYMBOLS.ILoginController).toClass(LoginController, [DI_SYMBOLS.ILoginUseCase]);
 }
