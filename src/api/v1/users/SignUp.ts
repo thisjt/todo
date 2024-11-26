8a1e3cd1130e1f60f014fa114c047c3d2c9bca73
Joshua Tahanlangit <contact@thisjt.me>
2024-11-26 23:08:59 +0800

    feat: sign up endpoint

---
+++ src/api/v1/users/SignUp.ts
@@ -0,0 +1,37 @@
+import type { OpenAPIHonoConfig } from '$lib/types';
+import { OpenAPIHono } from '@hono/zod-openapi';
+import * as StatusCodes from 'stoker/http-status-codes';
+import { setCookie } from 'hono/cookie';
+
+import { signupRouteHandler } from '$src/interface-adapter/controller/SignUp';
+
+import { getDI } from '$src/di/container';
+import { InvalidSignupToken } from '$src/domain/errors';
+
+const app = new OpenAPIHono<OpenAPIHonoConfig>();
+
+export const signupRoute = app.openapi(signupRouteHandler, async (c) => {
+	const requestData = c.req.valid('json');
+
+	const signupController = getDI('ISignUpController');
+
+	try {
+		const result = await signupController.execute(requestData);
+		if (result.token) {
+			setCookie(c, 'loginToken4', result.token, {
+				secure: true,
+				sameSite: 'strict',
+				httpOnly: true
+			});
+		}
+		c.var.logger.info(requestData.username, 'Signup Success');
+		return c.json({ success: true }, StatusCodes.CREATED);
+	} catch (error) {
+		if (error instanceof InvalidSignupToken) {
+			c.var.logger.info(error, 'Invalid Signup Token');
+			return c.json({ success: false }, StatusCodes.UNAUTHORIZED);
+		}
+		c.var.logger.error(error, 'Internal Server Error');
+		return c.json({ success: false }, StatusCodes.INTERNAL_SERVER_ERROR);
+	}
+});
