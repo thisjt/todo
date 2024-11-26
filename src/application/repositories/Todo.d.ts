d85418c7e80c13cc8f296eb5e6ccb7e86a5446fb
Joshua Tahanlangit <contact@thisjt.me>
2024-11-27 00:44:28 +0800

    feat: app todo repo

---
+++ src/application/repositories/Todo.d.ts
@@ -0,0 +1,9 @@
+import type { Todo } from '$src/domain/Todo';
+
+export interface ITodoRepository {
+	create(todo: Todo): Promise<void>;
+	find(id: number): Promise<Todo | null>;
+	findFromUsers(userId: number): Promise<Todo[]>;
+	update(todo: Todo): Promise<Todo>;
+	delete(id: number): Promise<void>;
+}
