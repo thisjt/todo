509bff52101ed389631b641c49eb6a0047d69c85
Joshua Tahanlangit <contact@thisjt.me>
2024-11-27 00:19:17 +0800

    refactor: remove pino pretty, update stoker

--- src/api/middleware/pino.ts
+++ src/api/middleware/pino.ts
@@ -1,25 +1,16 @@
 import type { OpenAPIHonoConfig } from '$lib/types';
 import type { OpenAPIHono } from '@hono/zod-openapi';
-import { dev } from '$app/environment';
 
 import { pinoLogger } from 'hono-pino';
 import pino from 'pino';
-import pretty from 'pino-pretty';
 import crypto from 'crypto';
 
 export function mountPinoLogger(app: OpenAPIHono<OpenAPIHonoConfig>) {
 	app.use(
 		pinoLogger({
-			pino: pino(
-				{
-					level: 'info'
-				},
-				dev
-					? pretty({
-							ignore: 'req.headers,err.stack'
-						})
-					: undefined
-			),
+			pino: pino({
+				level: 'info'
+			}),
 			http: {
 				reqId: () => crypto.randomUUID()
 			}
