53ad48aff21672501506004018db9d2c97748dbe
Joshua Tahanlangit <contact@thisjt.me>
2024-11-27 01:18:13 +0800

    feat: readtodousecase

---
+++ src/application/usecases/todo/Read.ts
@@ -0,0 +1,12 @@
+import type { ITodoRepository } from '$src/application/repositories/Todo';
+import { NotFound } from '$src/domain/errors';
+
+export class ReadTodoUseCase {
+	constructor(private todoRepository: ITodoRepository) {}
+
+	async execute(id: number) {
+		const todo = await this.todoRepository.find(id);
+		if (!todo) throw new NotFound('Todo Entry Does Not Exist');
+		return todo;
+	}
+}
