47c72c2edc92155610fdfc92c88b08499a908c83
Joshua Tahanlangit <contact@thisjt.me>
2024-12-01 15:38:57 +0800

    refactor: readmany route handler

--- src/interface-adapter/controller/todo/Read.ts
+++ src/interface-adapter/controller/todo/Read.ts
@@ -3,35 +3,49 @@ import { createRoute } from '@hono/zod-openapi';
 import { jsonContentRequired } from 'stoker/openapi/helpers';
 import * as StatusCodes from 'stoker/http-status-codes';
 
-import { IdParamsSchema } from "stoker/openapi/schemas";
-
 import { ReadTodoUseCase } from '$src/application/usecases/todo/Read';
 import { ReadManyTodoUseCase } from '$src/application/usecases/todo/ReadFromUser';
 
 import { TodoSchema } from './Create';
 import { CreateTodoSchema } from './Create';
 
-const ReadTodoSchemaInput = z.object({
-    id: z.coerce.number().optional().openapi({})
+export const ReadTodoSchemaInput = z.object({
+	id: z.coerce.number().optional().openapi({})
 });
 
-const ArrayCreateTodoSchema = z.array(CreateTodoSchema);
+export const ArrayCreateTodoSchema = z.array(CreateTodoSchema);
 
 const FailedResponse = z.object({
 	success: z.boolean().openapi({})
 });
 
 export const readTodoRouteHandler = createRoute({
-    method: 'get',
+	method: 'get',
 	path: '/api/v1/todo/{id}',
 	summary: 'Read Todo Entry',
 	description: 'Read a Todo Entry from the database',
 	request: {
-        params: IdParamsSchema
-    },
+		params: ReadTodoSchemaInput
+	},
 	responses: {
-		[StatusCodes.CREATED]: jsonContentRequired(TodoSchema, 'Created Todo Successfully'),
-		[StatusCodes.BAD_REQUEST]: jsonContentRequired(FailedResponse, 'Missing Fields'),
+		[StatusCodes.OK]: jsonContentRequired(TodoSchema, 'Read Todo Successfully'),
+		[StatusCodes.NOT_FOUND]: jsonContentRequired(FailedResponse, 'Not Found'),
+		[StatusCodes.UNAUTHORIZED]: jsonContentRequired(FailedResponse, 'Not Logged In'),
+		[StatusCodes.INTERNAL_SERVER_ERROR]: jsonContentRequired(
+			FailedResponse,
+			'Internal Server Error'
+		)
+	}
+});
+
+export const readManyTodoRouteHandler = createRoute({
+	method: 'get',
+	path: '/api/v1/todo',
+	summary: 'Read Todo Entry',
+	description: 'Read a Todo Entry from the database',
+	responses: {
+		[StatusCodes.OK]: jsonContentRequired(TodoSchema, 'Created Todo Successfully'),
+		[StatusCodes.NOT_FOUND]: jsonContentRequired(FailedResponse, 'Not Found'),
 		[StatusCodes.UNAUTHORIZED]: jsonContentRequired(FailedResponse, 'Not Logged In'),
 		[StatusCodes.INTERNAL_SERVER_ERROR]: jsonContentRequired(
 			FailedResponse,
@@ -41,13 +55,16 @@ export const readTodoRouteHandler = createRoute({
 });
 
 export class ReadTodoController {
-    constructor(private _readTodoUseCase: ReadTodoUseCase, private _readManyTodoUseCase: ReadManyTodoUseCase) {}
+	constructor(
+		private _readTodoUseCase: ReadTodoUseCase,
+		private _readManyTodoUseCase: ReadManyTodoUseCase
+	) {}
 
-    async execute(userId: number, id?: number) {
-        if (id) return _readTodoUseCase.execute(id);
+	async execute(userId: number, id?: number) {
+		if (id) return this._readTodoUseCase.execute(id);
 
-		return _readManyTodoUseCase.execute();
-    }
+		return this._readManyTodoUseCase.execute(userId);
+	}
 }
 
 export type IReadTodoController = typeof ReadTodoController.prototype;
