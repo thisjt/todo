a42dd03063b9e6060af85f0357a92b6ab26fd56d
Joshua Tahanlangit <contact@thisjt.me>
2024-12-05 03:33:57 +0800

    feat: update todo use case

--- src/application/usecases/todo/Update.ts
+++ src/application/usecases/todo/Update.ts
@@ -4,7 +4,7 @@ import type { Todo } from '$src/domain/Todo';
 export class UpdateTodoUseCase {
 	constructor(private todoRepository: ITodoRepository) {}
 
-	async execute(todo: Todo) {
+	async execute(todo: ReturnType<Todo['getData']>) {
 		const updatedTodo = await this.todoRepository.update(todo);
 		return updatedTodo;
 	}
