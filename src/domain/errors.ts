db6390c867ba5e0ec79819d325be95afba96c1d4
Joshua Tahanlangit <contact@thisjt.me>
2024-12-05 22:09:22 +0800

    refactor: modify error entities to add more context

--- src/domain/errors.ts
+++ src/domain/errors.ts
@@ -1,38 +1,69 @@
 export class DomainError extends Error {
-	constructor(message: string, options?: ErrorOptions) {
-		super(message, options);
+	constructor(
+		message: string,
+		public readonly context?: Record<string, unknown>
+	) {
+		super(message);
 		this.name = this.constructor.name;
 	}
 }
 
 export class ApplicationError extends Error {
-	constructor(message: string, options?: ErrorOptions) {
-		super(message, options);
+	constructor(
+		message: string,
+		public readonly context?: Record<string, unknown>
+	) {
+		super(message);
 		this.name = this.constructor.name;
 	}
 }
 export class AuthenticationError extends ApplicationError {}
-export class UserAlreadyExists extends ApplicationError {}
-export class NotFound extends ApplicationError {}
+export class AlreadyExists extends ApplicationError {
+	constructor(entity: string, identifier: string | number, context?: Record<string, unknown>) {
+		super(`Entity "${entity}" with the identifier "${identifier}" already EXISTS.`, {
+			entity,
+			identifier,
+			...context
+		});
+	}
+}
+export class NotFound extends ApplicationError {
+	constructor(entity: string, identifier: string | number, context?: Record<string, unknown>) {
+		super(`Entity "${entity}" with the identifier "${identifier}" does NOT EXIST.`, {
+			entity,
+			identifier,
+			...context
+		});
+	}
+}
 
 export class InterfaceAdapterError extends Error {
-	constructor(message: string, options?: ErrorOptions) {
-		super(message, options);
+	constructor(
+		message: string,
+		public readonly context?: Record<string, unknown>
+	) {
+		super(message);
 		this.name = this.constructor.name;
 	}
 }
 export class InvalidSignupToken extends InterfaceAdapterError {}
 
 export class PresentationError extends Error {
-	constructor(message: string, options?: ErrorOptions) {
-		super(message, options);
+	constructor(
+		message: string,
+		public readonly context?: Record<string, unknown>
+	) {
+		super(message);
 		this.name = this.constructor.name;
 	}
 }
 
 export class InfrastructureError extends Error {
-	constructor(message: string, options?: ErrorOptions) {
-		super(message, options);
+	constructor(
+		message: string,
+		public readonly context?: Record<string, unknown>
+	) {
+		super(message);
 		this.name = this.constructor.name;
 	}
 }
