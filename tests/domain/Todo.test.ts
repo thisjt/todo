8dc6a33144c63299b875a368d437360a06de9862
Joshua Tahanlangit <contact@thisjt.me>
2024-12-05 21:36:51 +0800

    test: todo domain entity test

---
+++ tests/domain/Todo.test.ts
@@ -0,0 +1,51 @@
+import { expect, describe, it } from 'vitest';
+
+import { Todo } from '$src/domain/Todo';
+
+describe('todo entity domain', () => {
+	const todoMockData = {
+		id: 11,
+		title: 'todo',
+		details: 'todo details',
+		completed: false,
+		userId: 3
+	};
+
+	const modifiedMockData = {
+		id: 9,
+		title: 'todo modified',
+		details: 'todo details modified',
+		completed: true,
+		userId: 4
+	};
+
+	describe('constructor', () => {
+		const todo = new Todo(todoMockData);
+
+		it('should create a todo entity with all the properties', () => {
+			expect(todo.id).toBe(todoMockData.id);
+			expect(todo.title).toBe(todoMockData.title);
+			expect(todo.details).toBe(todoMockData.details);
+			expect(todo.completed).toBe(todoMockData.completed);
+			expect(todo.userId).toBe(todoMockData.userId);
+		});
+	});
+
+	describe('functions', () => {
+		it('should return all values', () => {
+			const todo = new Todo(todoMockData);
+			expect(todo.getData()).toEqual(todoMockData);
+		});
+
+		it('should update only updatable values', () => {
+			const todo = new Todo(todoMockData);
+			todo.updateData(modifiedMockData);
+
+			expect(todo.getData()).toEqual({
+				...modifiedMockData,
+				id: todoMockData.id,
+				userId: todoMockData.userId
+			});
+		});
+	});
+});
