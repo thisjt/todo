bf87f02e82c139b76a3a12fded222ab00986bf1a
Joshua Tahanlangit <contact@thisjt.me>
2024-12-05 22:46:54 +0800

    test: error domain entity tests

---
+++ tests/domain/Errors.test.ts
@@ -0,0 +1,60 @@
+import { expect, it, describe } from 'vitest';
+
+import {
+	AlreadyExists,
+	ApplicationError,
+	AuthenticationError,
+	DomainError,
+	NotFound
+} from '$src/domain/Errors';
+
+describe('errors domain entity', () => {
+	const context = { test: 'value' };
+	const identifier = 5;
+	const entity = 'sampleEntity';
+
+	describe('domain errors', () => {
+		it('should contain base domain error', () => {
+			const error = new DomainError('Base Error');
+			expect(error.message).toBe('Base Error');
+			expect(error.name).toBe('DomainError');
+		});
+
+		it('should store context if available', () => {
+			const context = { test: 'value' };
+			const error = new DomainError('Base Error', context);
+			expect(error.context).toEqual(context);
+		});
+	});
+
+	describe('application errors', () => {
+		it('should contain base application error', () => {
+			const error = new ApplicationError('Base Error');
+			expect(error.message).toBe('Base Error');
+			expect(error.name).toBe('ApplicationError');
+		});
+
+		it('should store context if available', () => {
+			const error = new ApplicationError('Base Error', context);
+			expect(error.context).toEqual(context);
+		});
+
+		it('should be able to deliver AuthenticationError', () => {
+			const error = new AuthenticationError(identifier, context);
+			expect(error.context).toEqual({ identifier, ...context });
+			expect(error.name).toBe('AuthenticationError');
+		});
+
+		it('should be able to deliver AlreadyExists', () => {
+			const error = new AlreadyExists(entity, identifier, context);
+			expect(error.context).toEqual({ entity, identifier, ...context });
+			expect(error.name).toBe('AlreadyExists');
+		});
+
+		it('should be able to deliver NotFound', () => {
+			const error = new NotFound(entity, identifier, context);
+			expect(error.context).toEqual({ entity, identifier, ...context });
+			expect(error.name).toBe('NotFound');
+		});
+	});
+});
