5bbdaa56010b441e6728f44700af255f70204963
Joshua Tahanlangit <contact@thisjt.me>
2024-12-10 23:44:07 +0800

    refactor: inject todo repo to di container

--- src/di/types.ts
+++ src/di/types.ts
@@ -18,6 +18,7 @@ import type { IDeleteTodoUseCase } from '$src/application/usecases/todo/Delete';
 import type { IReadTodoUseCase } from '$src/application/usecases/todo/Read';
 import type { IReadManyTodoUseCase } from '$src/application/usecases/todo/ReadFromUser';
 import type { IUpdateTodoUseCase } from '$src/application/usecases/todo/Update';
+import type { ITodoRepository } from '$src/application/repositories/Todo';
 
 export const DI_SYMBOLS = {
 	Context: Symbol.for('Context'),
@@ -31,6 +32,8 @@ export const DI_SYMBOLS = {
 
 	ILoginController: Symbol.for('ILoginController'),
 	ISignUpController: Symbol.for('ISignUpController'),
+
+	ITodoRepository: Symbol.for('ITodoRepository'),
 
 	ICreateTodoController: Symbol.for('ICreateTodoController'),
 	IDeleteTodoController: Symbol.for('IDeleteTodoController'),
@@ -56,6 +59,8 @@ export interface DI_RETURN_TYPES {
 
 	ILoginController: ILoginController;
 	ISignUpController: ISignUpController;
+
+	ITodoRepository: ITodoRepository;
 
 	ICreateTodoController: ICreateTodoController;
 	IDeleteTodoController: IDeleteTodoController;
